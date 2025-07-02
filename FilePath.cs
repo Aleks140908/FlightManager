3495f4e5423a2f2ce5ff9643897cb5449d4fdbad
Asus <Asus@DESKTOP-2RHI8S2>
2025-07-02 23:14:47 +0300

    added

--- FilePath.cs
+++ FilePath.cs
@@ -9,5 +9,7 @@ namespace FlightManager
     public class FilePath
     {
         public const string FlightsFilePath = "flights.txt";
+        //different method for FilePath, changed the properties on flights.txt
+        //(Build Action->Content , Copy to Output Directory->Copy if newer)
     }
 }
