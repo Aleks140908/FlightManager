9e2843daacd39a5b02fdde71ff90a5b9018154ee
Ailqq <aylasali243@gmail.com>
2025-07-04 12:00:10 +0300

    .

--- FlightService.cs
+++ FlightService.cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace FlightManager
 {
     public class FlightService
     {
+        private readonly Data data;// only read from class data
+
+        public FlightService()
+        {
+            data = new Data();
+        }
         public void AddNewFlight()
         {
 
@@ -18,5 +25,15 @@ namespace FlightManager
             
 
         }
+        public void CheckAvailability()
+        {
+           
+        }
+
+        public void ShowAllFlights()
+        {
+            
+        }
+        
     }
 }
